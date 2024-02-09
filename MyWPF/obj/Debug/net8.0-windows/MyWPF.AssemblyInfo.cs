3d662c4c3c09bdeb30f96e1785dbfccc4adaa27c
frste <fredrik.stetler@knightec.se>
2024-02-09 10:25:18 +0100

    removed .github

--- MyWPF/obj/Debug/net8.0-windows/MyWPF.AssemblyInfo.cs
+++ MyWPF/obj/Debug/net8.0-windows/MyWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2326c235a5ad96274b142cdddd767a92f9fca69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091c0206879f3c288f7c1dc362977aa28d98b739")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
