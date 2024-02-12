ee7f9f52680f5008e785a81894b1277193002515
frste <fredrik.stetler@knightec.se>
2024-02-12 12:16:07 +0100

    saved

--- MyWPF/obj/Debug/net8.0-windows/MyWPF.AssemblyInfo.cs
+++ MyWPF/obj/Debug/net8.0-windows/MyWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d662c4c3c09bdeb30f96e1785dbfccc4adaa27c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c929617490c741b3048a5d9500567eb4334f76df")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
